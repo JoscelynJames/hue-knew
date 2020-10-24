2a7528b5d95374906dcb3762b74bad9fab5e6ef3
Joscelyn James <josce.james7@hotmail.com>
2020-10-24 17:52:53 -0600

    feat: added base buttom component

---
+++ src/atoms/Button.vue
@@ -0,0 +1,45 @@
+<template>
+  <button type="button" :name="name">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    name: String,
+  },
+};
+</script>
+
+<style scoped>
+button {
+  height: 64px;
+  min-width: 200px;
+  background: white;
+  border-radius: 32px;
+  border: 3px solid var(--primary-color);
+  color: var(--primary-color);
+  font-weight: bold;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:hover {
+  color: var(--primary-dark-color);
+  border-color: var(--primary-dark-color);
+}
+
+button:focus {
+  outline: none;
+  box-shadow: var(--button-focus);
+}
+
+button:active {
+  background: var(--primary-light-color);
+  color: var(--primary-dark-color);
+  border-color: var(--primary-dark-color);
+}
+</style>
