80acba34b852c7d6c06a594a7413e1c613e95e24
Joscelyn James <josce.james7@hotmail.com>
2020-10-24 16:38:06 -0600

    feat: added Nav Bar component

--- src/pages/Generate.vue
+++ src/pages/Generate.vue
@@ -1,5 +1,6 @@
 <template>
   <main id="generate-page">
+    <nav-bar />
     <!-- This will be the canvas to draw the image -->
     <canvas id="image-canvas"></canvas>
     <!-- This will be the svg we generate and draw our colors onto -->
@@ -8,7 +9,12 @@
 </template>
 
 <script>
+import NavBar from '../templates/NavBar.vue'
+
 export default {
-  name: 'Generate'
+  name: 'Generate',
+  components: {
+    'nav-bar': NavBar 
+  }
 }
 </script>
---
+++ src/templates/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <h2>Color Pop</h2>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style scoped>
+nav {
+  text-align: center;
+  height: 72px;
+  box-shadow: var(--box-shadow-1);
+}
+</style>
