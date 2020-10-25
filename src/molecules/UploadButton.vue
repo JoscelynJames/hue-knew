2a24117a8af93102f2e65755dac250e20d4421f3
Joscelyn James <josce.james7@hotmail.com>
2020-10-24 18:39:53 -0600

    feat: added upload button component

---
+++ src/molecules/UploadButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="file" id="image-upload" hidden/>
+  <app-button @click="handleButtonClick()" text="UPLOAD" name="upload-button"/>
+</template>
+
+<script>
+import Button from "../atoms/Button.vue";
+
+export default {
+  name: "Generate",
+  components: {
+    "app-button": Button,
+  },
+  methods: {
+    handleButtonClick() {
+      document.getElementById('image-upload').click()
+    }
+  }
+};
+</script>
+
