8bdf958bd0d5f682d8e0ead62d51721f41815f94
Joscelyn James <josce.james7@hotmail.com>
2020-10-24 18:48:50 -0600

    refactor: moved nav bar into app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="color-gen-app">
+    <nav-bar />
     <generate />
   </div>
 </template>
 
 <script>
 import Generate from "./pages/Generate.vue";
+import NavBar from "./templates/NavBar.vue";
+
 export default {
   name: "App",
   components: {
     generate: Generate,
+    'nav-bar': NavBar,
   },
 };
 </script>
--- src/pages/Generate.vue
+++ src/pages/Generate.vue
@@ -1,6 +1,9 @@
 <template>
   <main id="generate-page">
-    <nav-bar />
+    <section>
+      <upload-button text="UPLOAD" name="upload-button"/>
+      <!-- TODO feature: add a save button here to save the colors in json, csv or the svg  -->
+    </section>
     <!-- This will be the canvas to draw the image -->
     <canvas id="image-canvas"></canvas>
     <!-- This will be the svg we generate and draw our colors onto -->
@@ -9,12 +12,20 @@
 </template>
 
 <script>
-import NavBar from "../templates/NavBar.vue";
+import Button from "../molecules/UploadButton.vue";
 
 export default {
   name: "Generate",
   components: {
-    "nav-bar": NavBar,
+    "upload-button": Button,
   },
 };
 </script>
+
+<style scoped>
+#generate-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
