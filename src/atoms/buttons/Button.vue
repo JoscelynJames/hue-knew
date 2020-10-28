25e11028195d6ab25c74562e55c99f4532d80e94
Joscelyn James <josce.james7@hotmail.com>
2020-10-27 18:21:43 -0600

    task: made buttons folder

---
+++ src/atoms/buttons/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="outlined-button" type="button" v-bind:name="name">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    name: String,
+    loading: Boolean,
+  },
+};
+</script>
+
+<style >
+@import url("./Button.css");
+</style>
