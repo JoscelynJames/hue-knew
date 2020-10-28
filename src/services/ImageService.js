1077a10cf5719cf87e0c9c1af89f1f97ce23e7ca
Joscelyn James <josce.james7@hotmail.com>
2020-10-27 19:10:04 -0600

    task: added image service

---
+++ src/services/ImageService.js
@@ -0,0 +1,39 @@
+export class ImageService {
+  imageUrl
+  context
+
+  constructor(image, context) {
+    this.validateImage(image)
+
+    this.imageUrl = this.createImageUrl(image);
+    this.context = context;
+  }
+
+  openFileExplorer() {
+    document.getElementById("image-upload").click();
+  }
+
+  createImageUrl(image) { 
+    return URL.createObjectURL(image);
+  }
+
+  validateImage(image) {
+    // TODO: add better validation here and user feedback
+    if (!image) throw Error('No image was provided')
+  }
+
+  drawImage() {
+    return new Promise((res, rej) => {
+      // create a new HTMLImageElement for drawImage
+      const img = new Image(100, 100);
+      img.src = this.imageUrl;
+      // wait for the image to load otherwise it paints a blank img
+      img.onload = () => {
+        this.context.drawImage(img, 0, 0, 100, 100);
+        const imageData = this.context.getImageData(0, 0, 100, 100);
+
+        res(imageData)
+      }
+    })
+  }
+}
