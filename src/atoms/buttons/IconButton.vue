72fd0572b912d13abb0407477759b44b5992e2ab
Joscelyn James <josce.james7@hotmail.com>
2020-10-27 18:21:09 -0600

    feat: added icon button

---
+++ src/atoms/buttons/IconButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="outlined-button" type="button" :name="name">
+    <img class="loading-animation" :src="src" :alt="alt"/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "IconButton",
+  props: {
+    loading: Boolean,
+    src: String,
+    alt: String,
+  },
+};
+</script>
+
+<style >
+@import url("./Button.css");
+</style>
