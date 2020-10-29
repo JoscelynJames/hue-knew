49ae6a7f4bdca9321e93f3735a9d39779a334336
Joscelyn James <josce.james7@hotmail.com>
2020-10-29 08:43:28 -0600

    feature: added flat button component

---
+++ src/atoms/buttons/FlatButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="flat-button" :name="name">
+      <span>{{ text  }}</span>
+    </button>
+</template>
+
+<script>
+export default {
+  name: "FlatButton",
+  props: {
+    text: String,
+    name: String,
+  },
+};
+</script>
+
+<style >
+.flat-button {
+  background: none;
+  color: white;
+  border: none;
+  padding: 5px;
+  border-radius: 5px;
+  opacity: .5;
+}
+
+.flat-button:hover {
+  cursor: pointer;
+  background: var(--primary-mid-color);
+}
+</style>
